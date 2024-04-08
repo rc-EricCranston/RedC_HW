17eb6c62be498551390d877f2c466eafc8f272bb
Ga-Young Jin <gyjinn@gmail.com>
2024-04-07 21:47:39 -0700

    Add activity to modify a file

--- main.rb
+++ main.rb
@@ -3,12 +3,19 @@ def main(input_file)
   File.foreach(input_file) do |line|
     line.chomp!
     line_args = line.split(",")
-    if line_args[0] == "create"
+    case line_args[0]
+    when "create"
       create_file(line, line_args[1], line_args[2])
+    when "modify"
+      modify_file(line, line_args[1], line_args[2])
     end
   end
 end
 
 def create_file(command, path, file_name)
   `mkdir -p #{path} && touch #{path}#{file_name}`
+end
+
+def modify_file(command, file, added_text)
+  `echo #{added_text} >> #{file}`
 end
