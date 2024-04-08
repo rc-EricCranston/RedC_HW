bd8a58e82a09111f4aa5fdef90721fbae54a56dd
Ga-Young Jin <gyjinn@gmail.com>
2024-04-07 21:42:34 -0700

    Add activity to create a new file

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 def main(input_file)
   puts input_file
   File.foreach(input_file) do |line|
-    puts line
+    line.chomp!
+    line_args = line.split(",")
+    if line_args[0] == "create"
+      create_file(line, line_args[1], line_args[2])
+    end
   end
+end
+
+def create_file(command, path, file_name)
+  `mkdir -p #{path} && touch #{path}#{file_name}`
 end
