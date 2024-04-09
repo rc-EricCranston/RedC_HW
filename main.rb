af1a0f305e2a247c81595778ffd076a5fb18dacb
Ga-Young Jin <gyjinn@gmail.com>
2024-04-09 11:17:12 -0700

    Add create file activity log

--- main.rb
+++ main.rb
@@ -37,13 +37,38 @@ def main(input_file)
   end
 end
 
-def create_file(line, activity, path, file_name)
-  # Make the directory if it doesn't exist
-  Dir.mkdir(path) unless Dir.exist?(path)
-  
-  # Make the file if it doesn't exist
-  File.open(path+file_name, 'a+') do |file|
-    file.write()
+def create_file(line, activity, dir_path, file_name)
+  # Make the directory and file, rescue if error
+  begin
+    Dir.mkdir(dir_path) unless Dir.exist?(dir_path)
+    
+    File.open(dir_path+file_name, 'a+') do |file|
+      file.write()
+    end
+    
+    # Add a file activity log
+    ruby_process = Sys::ProcTable.ps(pid: Process.pid)
+    
+    create_file_activity_log = 
+    { timestamp: Time.now,
+    file_path: dir_path+file_name,
+    activity: activity,
+    username: ruby_process.environ["USER"],
+    process_name: ruby_process.comm,
+    process_command_line: ruby_process.cmdline,
+    process_id: ruby_process.pid }
+    
+    log_path = './logs.json'
+    
+    existing_logs = File.read(log_path) if File.exist?(log_path)
+    logs_data = existing_logs ? JSON.parse(existing_logs) : []
+    logs_data << create_file_activity_log
+    updated_json = JSON.pretty_generate(logs_data)
+    File.open(log_path, 'w') do |file|
+      file.write(updated_json)
+    end
+  rescue Errno::ENOENT => e
+    puts "Error: #{e.message}"
   end
 end
 
