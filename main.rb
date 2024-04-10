b82de5148eb270fff64be469f97203fb04365c8b
Ga-Young Jin <gyjinn@gmail.com>
2024-04-09 17:33:50 -0700

    Add activity to establish a network connection and transmit data

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'socket'
 require 'sys/proctable'
 
 def main(input_file)
@@ -33,6 +34,8 @@ def main(input_file)
       modify_file(line, line_args[0], line_args[1], line_args[2])
     when "delete"
       delete_file(line, line_args[0], line_args[1])
+    when "network connection"
+      network_connection(line, line_args[0], line_args[1], line_args[2], line_args[3])
     end
   end
 end
@@ -136,5 +139,58 @@ def delete_file(command, activity, file_path)
     end
   rescue Errno::ENOENT => e
     puts "Error: #{e.message}"
+  end
+  
+  def network_connection(command, activity, server_address, server_port, file_path)
+    port = server_port.to_i
+    file_size = File.size(file_path)
+    
+    # Connect to the server
+    client = TCPSocket.new(server_address, port)
+    
+    # Get the source and destination address and port
+    local_address = client.addr
+    remote_address = client.peeraddr
+    
+    # Send the file size to the server
+    client.puts file_size.to_s
+    
+    # Read the file and send to the server
+    File.open(file_path, 'rb') do |file|
+      while chunk = file.read(1024)
+        client.write(chunk)
+      end
+    end
+    
+    # Receive confirmation from the server
+    confirmation = client.gets.chomp
+    puts "Server response: #{confirmation}"
+    
+    # Close the connection to the server
+    client.close
+    
+    # Add a network connection activity log
+    ruby_process = Sys::ProcTable.ps(pid: Process.pid)
+    
+    network_connection_file_activity_log = 
+    { timestamp: Time.now,
+    username: ruby_process.environ["USER"],
+    destination: "#{remote_address[3]}:#{remote_address[1]}",
+    source: "#{local_address[3]}:#{local_address[1]}",
+    amount_of_data: file_size,
+    protocol: "#{local_address[0]}",
+    process_name: ruby_process.comm,
+    process_command_line: ruby_process.cmdline,
+    process_id: ruby_process.pid }
+    
+    log_path = './logs.json'
+    
+    existing_logs = File.read(log_path) if File.exist?(log_path)
+    logs_data = existing_logs ? JSON.parse(existing_logs) : []
+    logs_data << network_connection_file_activity_log
+    updated_json = JSON.pretty_generate(logs_data)
+    File.open(log_path, 'w') do |file|
+      file.write(updated_json)
+    end
   end
 end
---
+++ server_side/tcp_server.rb
@@ -0,0 +1,47 @@
+require 'socket'
+
+# Define the port number to listen on
+port = 3000
+
+# Create a server
+server = TCPServer.new(port)
+
+puts "Server is listening on port #{port}"
+
+# File name counter
+file_num = 1
+# Accept incoming connections
+loop do
+  client = server.accept
+  
+  # Handle each client connection in a separate thread
+  Thread.new(client) do |c|
+    # Receive the file size from the client
+    file_size_str = c.gets.chomp
+    file_size = file_size_str.to_i
+    
+    # Receive and save the file data
+    begin
+      file_path = "server_side/received_files/file_#{file_num}.txt"
+      File.open(file_path, 'wb') do |file|
+        bytes_received = 0
+        while bytes_received < file_size
+          chunk = c.read([1024, file_size - bytes_received].min)
+          break if chunk.nil?  # Handle unexpected EOF or connection closure
+          bytes_received += chunk.length
+          file.write(chunk)
+        end
+      end
+      file_num += 1
+      # Send confirmation to client
+      c.puts "File received."
+    rescue Errno::ENOENT => e
+      # Send error to client
+      c.puts "Error: #{e.message}"
+    end
+    
+    # Close the connection to the client
+    puts "Client disconnected"
+    c.close
+  end
+end
