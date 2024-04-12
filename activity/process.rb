fc6b5a5f10ff1105e4864749976316924ad48d10
Ga-Young Jin <gyjinn@gmail.com>
2024-04-11 19:06:24 -0700

    Add begin/rescue to starting a process activity

--- activity/process.rb
+++ activity/process.rb
@@ -1,9 +1,13 @@
 def open_app(command, activity, application)
-  # Spawn new process to open the application
-  open_app_command = "open -a #{application}"
-  app_pid = Process.spawn(open_app_command)
-  app_process = Sys::ProcTable.ps(pid: app_pid)
-  
-  # Add a process activity log
-  create_process_log(app_process)
+  # Spawn new process to open the application, rescue if error
+  begin
+    open_app_command = "open -a #{application}"
+    app_pid = Process.spawn(open_app_command)
+    app_process = Sys::ProcTable.ps(pid: app_pid)
+    
+    # Add a process activity log
+    create_process_log(app_process)
+  rescue Errno::ENOENT => e
+    puts "Error: #{e.message}"
+  end
 end
